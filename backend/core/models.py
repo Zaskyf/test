292967298199a05c5be4d85aa7bd7e5d6117d560
alexkhub <aleksandrkhubaev04@gmail.com>
2025-01-17 20:30:24 +0300

    start backend + create models

---
+++ backend/core/models.py
@@ -0,0 +1,135 @@
+from django.contrib.auth.hashers import make_password
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from sortedm2m.fields import SortedManyToManyField
+
+
+class User(AbstractUser):
+    salary = models.PositiveIntegerField(default=0, verbose_name='Заработная плата')
+
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+    def save(self, *args, **kwargs):
+        if self.password and not self.password.startswith(('pbkdf2_sha256$', 'bcrypt$', 'argon2')):
+            self.password = make_password(self.password)
+
+        super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=150, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Image(models.Model):
+    image = models.ImageField(verbose_name='Картинка', upload_to='images/%Y/%m/%d/')
+
+    class Meta:
+        verbose_name = 'Картинка'
+
+
+class Good(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=150, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='Категория', null=True, blank=True)
+    images = SortedManyToManyField(Image, verbose_name='Картинки')
+    start_price = models.PositiveIntegerField(verbose_name='Начальная цена')
+    discount = models.DecimalField(verbose_name='Скидка', max_digits=5, decimal_places=2,
+                                   blank=True, null=True, default=0)
+    price = models.PositiveIntegerField(verbose_name='Цена', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.price = int(self.start_price - (self.start_price * (self.discount / 100)))
+        super(Good, self).save(*args, **kwargs)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    rating = models.PositiveIntegerField(verbose_name='Рейтинг')
+    good = models.ForeignKey(Good, on_delete=models.CASCADE, verbose_name='Продукт', blank=True, null=True)
+    text = models.TextField(verbose_name='Сообщение', blank=True, null=True)
+    created = models.DateTimeField(verbose_name='Дата и время создания', auto_now_add=True,)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    good = models.ForeignKey(Good, on_delete=models.CASCADE, verbose_name='Продукт', blank=True, null=True)
+    quantity = models.PositiveIntegerField(verbose_name='Количество', default=1)
+    price = models.PositiveIntegerField(verbose_name='Цена', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
+
+
+    def __str__(self):
+        return f'{self.good} x {self.quantity}'
+
+
+    def save(self, *args, **kwargs):
+        self.price = self.good.price * self.quantity
+        super(Cart, self).save(*args, **kwargs)
+
+
+
+
+class OrderPoint(models.Model):
+    good = models.ForeignKey(Good, on_delete=models.CASCADE, verbose_name='Продукт', blank=True, null=True)
+    quantity = models.PositiveIntegerField(verbose_name='Количество', default=1)
+    price = models.PositiveIntegerField(verbose_name='Цена', blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Пункт заказа"
+        verbose_name_plural = "Пункты заказов"
+
+    def save(self, *args, **kwargs):
+        self.price = self.good.price * self.quantity
+        super(OrderPoint, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.good} x {self.quantity}'
+
+
+
+class Order(models.Model):
+    status_field = (
+        ("Не оплачено", "Не оплачено"),
+        ("Оплачено", "Оплачено"),
+
+    )
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='Пользователь', null=True, blank=True)
+    order_points = SortedManyToManyField(OrderPoint, verbose_name="Пункт заказа")
+    price = models.PositiveIntegerField(blank=True, null=True, verbose_name='Цена заказа', )
+    status = models.CharField(max_length=50, verbose_name='Статус', default='Не оплачено', choices=status_field)
+    created = models.DateTimeField(verbose_name='Дата и время создания', auto_now_add=True, )
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return str(self.pk)
